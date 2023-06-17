ca1d415fd4bfb54c50ee182ae34c6c6beea84ce9
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-06-17 20:12:01 -0300

    Updated browsaer control

---
+++ js/controleDeBrowser.js
@@ -0,0 +1,20 @@
+var nomeDoBrowser = (function (agent) {        
+    switch (true) {
+        case agent.indexOf("edge") > -1: return "MS Edge";
+        case agent.indexOf("edg/") > -1: return "Edge (chromium based)";
+        case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+        case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+        case agent.indexOf("trident") > -1: return "MS IE";
+        case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
+        case agent.indexOf("safari") > -1: return "Safari";
+        default: return "other";
+    }
+})(window.navigator.userAgent.toLowerCase());
+
+if(nomeDoBrowser != "Chrome"){
+    document.body.innerHTML = `
+        <div class="main">
+            <h1 class="subtitle">Este site só funciona com o Google Chrome</h1>
+        </div>
+    `
+}
